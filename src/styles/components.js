35b3f4a587d7d04fbfdac630f962f54aacf42d71
maximakymenko <maks.akymenko@gmail.com>
2019-04-16 08:56:31 +0200

    adding function for generating links and styles

--- src/styles/components.js
+++ src/styles/components.js
@@ -5,9 +5,24 @@ export const StyledHeader = styled.header`
   font-size: 2rem;
   font-weight: bold;
   background: ${props => props.theme.red};
+
+  h1 {
+    font-size: ${props => props.theme.fs1};
+  }
 `;
 
 export const MovieContainer = styled.div`
+  display: flex;
+  margin: 2rem;
   border: 1px solid white;
-  background: red;
+
+  img {
+    border-radius: 0 1.5rem 1.5rem 0;
+    object-fit: contain;
+  }
+
+  h3 {
+    font-size: ${props => props.theme.fs2};
+    text-align: center;
+  }
 `;
---
+++ src/utils/createImageLink.js
@@ -0,0 +1,1 @@
+export const createImageLink = path => `https://image.tmdb.org/t/p/w300_and_h450_bestv2${path}`;
